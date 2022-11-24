887d0d9da9bd80bf72dbb8789cfb58b3c127aeb6
Qiusheng Wu <giswqs@gmail.com>
2022-11-23 21:48:47 -0500

    Fixed piplite import bug

--- leafmap_lite/__init__.py
+++ leafmap_lite/__init__.py
@@ -5,22 +5,21 @@ __email__ = 'giswqs@gmail.com'
 __version__ = '0.0.4'
 
 import asyncio
+import sys
 
 
 async def is_jupyterlite():
     """Check if the current environment is JupyterLite."""
 
-    import sys
     import piplite
 
-    if "pyodide" in sys.modules:
-
-        await piplite.install('leafmap', deps=False)
-        await piplite.install('shapely', deps=False)
-        await piplite.install('pyproj', deps=False)
-        await piplite.install('geopandas', deps=False)
+    await piplite.install('leafmap', deps=False)
+    await piplite.install('shapely', deps=False)
+    await piplite.install('pyproj', deps=False)
+    await piplite.install('geopandas', deps=False)
 
 
-asyncio.run(is_jupyterlite())
+if "pyodide" in sys.modules:
+    asyncio.run(is_jupyterlite())
 
     
