cad088cbad63086718cd095fc5a74d25e07e5350
Qiusheng Wu <giswqs@gmail.com>
2022-11-23 21:45:47 -0500

    Added async and await

--- leafmap_lite/__init__.py
+++ leafmap_lite/__init__.py
@@ -3,3 +3,24 @@
 __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
 __version__ = '0.0.3'
+
+import asyncio
+
+
+async def is_jupyterlite():
+    """Check if the current environment is JupyterLite."""
+
+    import sys
+    import piplite
+
+    if "pyodide" in sys.modules:
+
+        await piplite.install('leafmap', deps=False)
+        await piplite.install('shapely', deps=False)
+        await piplite.install('pyproj', deps=False)
+        await piplite.install('geopandas', deps=False)
+
+
+asyncio.run(is_jupyterlite())
+
+    
