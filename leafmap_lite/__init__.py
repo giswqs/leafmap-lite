d4273d6a9e1615dcd8abfcbeae134471f136a8bb
Qiusheng Wu <giswqs@gmail.com>
2022-11-24 00:26:55 -0500

    Removed pyodide

--- leafmap_lite/__init__.py
+++ leafmap_lite/__init__.py
@@ -4,22 +4,22 @@ __author__ = """Qiusheng Wu"""
 __email__ = 'giswqs@gmail.com'
 __version__ = '0.0.7'
 
-import sys
+# import sys
 
 
-async def install_pkgs():
-    """Check if the current environment is JupyterLite."""
+# async def install_pkgs():
+#     """Check if the current environment is JupyterLite."""
 
-    import piplite
+#     import piplite
 
-    await piplite.install('leafmap', deps=False)
-    await piplite.install('shapely', deps=False)
-    await piplite.install('pyproj', deps=False)
-    await piplite.install('geopandas', deps=False)
+#     await piplite.install('leafmap', deps=False)
+#     await piplite.install('shapely', deps=False)
+#     await piplite.install('pyproj', deps=False)
+#     await piplite.install('geopandas', deps=False)
 
     
-if "pyodide" in sys.modules:
-    # pyodide doesn't support numpy, so we need to install it separately
-    # and then import it.
-    import pyodide
-    pyodide.runPython("import piplite; piplite.install('leafmap', deps=False)")
+# if "pyodide" in sys.modules:
+#     # pyodide doesn't support numpy, so we need to install it separately
+#     # and then import it.
+#     import pyodide
+#     pyodide.runPython("import piplite; piplite.install('leafmap', deps=False)")
